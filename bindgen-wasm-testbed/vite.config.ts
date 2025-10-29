c6498b296c43ffbb0d592c69b05cfc25d7f20bdb
drumath2237 <2237k.drumath@gmail.com>
2025-10-30 04:03:43 +0900

    :green_heart: settings for gh pages

--- bindgen-wasm-testbed/vite.config.ts
+++ bindgen-wasm-testbed/vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from "vite";
 import wasm from "vite-plugin-wasm";
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
   return {
     plugins: [
       wasm(),
     ],
+    base: mode === "production"
+      ? "/sogloader-rust-wasm-bindgen-testbed/"
+      : undefined,
   };
 });
